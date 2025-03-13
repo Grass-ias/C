7c8f77b5a1c87a13f9a78cef858091f57412c766
Grass-ias <ferdyprasetyaputra08@gmail.com>
2025-03-13 16:01:43 +0700

    BilSempN.c

--- Alpro Week 3/BilSempN.c
+++ Alpro Week 3/BilSempN.c
@@ -5,54 +5,34 @@
 
 #include <stdio.h> /*Header file*/
 
-/*Notasi Algoritmik*/
-/*Program : BilSempN*/
-/*Menampilkan semua bilangan sempurna sampai N*/
-
-/*Kamus*/
-/*N : integer   { Batas bilangan }*/
-/*i, j : integer   { Counter untuk iterasi }*/
-/*jumlah_faktor : integer   { Menyimpan jumlah faktor bilangan }*/
-
-/*Algoritma*/
-/*input(N)*/
-/*for i = 1 to N do*/
-/*    jumlah_faktor = 0*/
-/*    for j = 1 to i-1 do*/
-/*        if (i mod j = 0) then*/
-/*            jumlah_faktor = jumlah_faktor + j*/
-/*    if jumlah_faktor = i then*/
-/*        output(i)*/
-
 int main() {
-    /*Kamus*/
-    int N, i, j, jumlah_faktor;
-
-    /*Algoritma*/
-    printf("Masukkan bilangan N: ");
+    /* Kamus */
+    int N, i = 2, j, sum;
+    
+    /* Algoritma */
+    printf("Masukkan nilai N: ");
     scanf("%d", &N);
 
     if (N <= 0) {
-        printf("N harus lebih besar dari 0!\n");
-        return 1;
-    }
-
-    printf("Bilangan Sempurna sampai %d adalah: ", N);
-
-    for (i = 1; i <= N; i++) {
-        jumlah_faktor = 0;
-
-        for (j = 1; j < i; j++) {
-            if (i % j == 0) {
-                jumlah_faktor += j;
+        printf("Masukan harus lebih dari 0\n");
+    } else {
+        printf("Bilangan Sempurna hingga %d adalah: ", N);
+        while (i <= N) {
+            sum = 0;
+            j = 1;
+            while (j < i) {
+                if (i % j == 0) {
+                    sum += j;
+                }
+                j++;
             }
+            if (sum == i) {
+                printf("%d ", i);
+            }
+            i++;
         }
-
-        if (jumlah_faktor == i) {
-            printf("%d ", i);
-        }
+        printf("\n");
     }
-
-    printf("\n");
+    
     return 0;
 }
