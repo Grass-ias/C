10e0b972a0057d53d53d28ab005396546d3d11de
Grass-ias <ferdyprasetyaputra08@gmail.com>
2025-03-13 12:13:55 +0700

    JumDeret.c

--- Alpro Week 3/JumDeret.c
+++ Alpro Week 3/JumDeret.c
@@ -1,9 +1,27 @@
-/*Nama File    : JumDeret.c*/
-/*Deskripsi    : Menghitung jumlah total dari deret bilangan 1 + 2 + ... + N*/
-/*Pembuat      : 24060124140145/Ferdy Prasetya Putra*/
-/*Tgl Pembuatan: 06/03/2025 08:15*/
+/*Nama File     : JumDeret.c*/
+/*Deskripsi     : Menghitung jumlah total dari deret bilangan 1 + 2 + ... + N*/
+/*Pembuat       : 24060124140145/Ferdy Prasetya Putra*/
+/*Tgl Pembuatan : 06/03/2025 08:15*/
 
-#include <stdio.h>
+#include <stdio.h> /*Header file*/
+
+/*Notasi Algoritmik*/
+/*Program : JumDeret*/
+/*Menjumlahkan deret bilangan dari 1 sampai N*/
+
+/*Kamus*/
+/*N : integer   { Batas bilangan }*/
+/*i : integer   { Counter untuk iterasi }*/
+/*jumlah : integer   { Menyimpan hasil penjumlahan }*/
+
+/*Algoritma*/
+/*input(N)*/
+/*jumlah = 0*/
+/*i = 1*/
+/*while i <= N do*/
+/*    jumlah = jumlah + i*/
+/*    i = i + 1*/
+/*output(jumlah)*/
 
 int main() {
     /*Kamus*/
