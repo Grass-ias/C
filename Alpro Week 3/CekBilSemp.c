c4e19a9cffa21eb68910ee570a6b871fa13a87bd
Grass-ias <ferdyprasetyaputra08@gmail.com>
2025-03-14 10:09:30 +0700

    CekBilSemp.c

--- Alpro Week 3/CekBilSemp.c
+++ Alpro Week 3/CekBilSemp.c
@@ -5,28 +5,6 @@
 
 #include <stdio.h> /*Header file*/
 
-/*Notasi Algoritmik*/
-/*Program : CekBilSemp*/
-/*Menentukan apakah bilangan N adalah bilangan sempurna*/
-
-/*Kamus*/
-/*N : integer   { Bilangan yang akan diperiksa }*/
-/*i : integer   { Counter untuk mencari faktor }*/
-/*JF : integer   { Menyimpan jumlah faktor bilangan (kecuali N sendiri) }*/
-
-/*Algoritma*/
-/*input(N)*/
-/*JF = 0*/
-/*i = 1*/
-/*while i < N do*/
-/*    if (N mod i = 0) then*/
-/*        JF = JF + i*/
-/*    i = i + 1*/
-/*if JF = N then*/
-/*    output("Bilangan Sempurna")*/
-/*else*/
-/*    output("Bukan Bilangan Sempurna")*/
-
 int main() {
     /*Kamus*/
     int N, i, JF;
@@ -39,20 +17,19 @@ int main() {
 
     if (N <= 0) {
         printf("N harus lebih besar dari 0!\n");
-        return 1;
-    }
-
-    while (i < N) {
-        if (N % i == 0) {
-            JF += i;
-        }
-        i++; // Increment i
-    }
-
-    if (JF == N) {
-        printf("%d adalah Bilangan Sempurna\n", N);
     } else {
-        printf("%d adalah Bukan Bilangan Sempurna\n", N);
+        while (i < N) {
+            if (N % i == 0) {
+                JF += i;
+            }
+            i++; 
+        }
+
+        if (JF == N) {
+            printf("%d adalah Bilangan Sempurna\n", N);
+        } else {
+            printf("%d adalah Bukan Bilangan Sempurna\n", N);
+        }
     }
 
     return 0;
