8676ecedd5bebeb5d37293ca15ed342c0e7dad09
Grass-ias <ferdyprasetyaputra08@gmail.com>
2025-03-14 10:08:10 +0700

    FaktorBil.c

--- Alpro Week 3/FaktorBil.c
+++ Alpro Week 3/FaktorBil.c
@@ -5,22 +5,6 @@
 
 #include <stdio.h> /*Header file*/
 
-/*Notasi Algoritmik*/
-/*Program : FaktorBil*/
-/*Menentukan faktor bilangan dari N*/
-
-/*Kamus*/
-/*N : integer   { Bilangan yang akan dicari faktornya }*/
-/*i : integer   { Counter untuk mencari faktor }*/
-
-/*Algoritma*/
-/*input(N)*/
-/*i = 1*/
-/*while i <= N do*/
-/*    if (N mod i = 0) then*/
-/*        output(i)*/
-/*    i = i + 1*/
-
 int main() {
     /*Kamus*/
     int N, i;
@@ -31,19 +15,18 @@ int main() {
 
     if (N <= 0) {
         printf("N harus lebih besar dari 0!\n");
-        return 1;
-    }
+    } else {
+        printf("Faktor bilangan dari %d adalah:\n", N);
+        i = 1;
 
-    printf("Faktor bilangan dari %d adalah:\n ", N);
-    i = 1; 
-
-    while (i <= N) {
-        if (N % i == 0) {
-            printf("%d ", i);
+        while (i <= N) {
+            if (N % i == 0) {
+                printf("%d ", i);
+            }
+            i++; 
         }
-        i++; // Increment i
+        printf("\n");
     }
 
     return 0;
 }
-
